bdc67e6ce3ee3262752ab76b69155ad51651bb93
num5268 <num5268@gmail.com>
2021-05-11 16:41:46 +0900

    server

--- Server_008_Hello/src/main/java/com/callor/hello/controller/MyPageController.java
+++ Server_008_Hello/src/main/java/com/callor/hello/controller/MyPageController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.callor.hello.model.MemberVO;
+
 @WebServlet("/mypage/*")
 public class MyPageController extends HttpServlet{
 
