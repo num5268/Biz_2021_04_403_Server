940e9b6ed72e97f2674b8f042ecd844c4adf9901
num5268 <num5268@gmail.com>
2021-05-27 16:42:12 +0900

    05-27

---
+++ Server_014_TodoListV1/src/main/java/com/callor/todo/model/TodoVO.java
@@ -0,0 +1,12 @@
+package com.callor.todo.model;
+
+public class TodoVO {
+	
+	private Long td_seq;
+	private String td_sdate;
+	private String td_stime;
+	private String td_doit;
+	private String td_edate;
+	private String td_etime;
+
+}
